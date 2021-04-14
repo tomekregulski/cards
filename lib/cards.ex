2a5c85d65b614a0732a1b58a99734ac43a6a7f32
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-04-14 10:42:53 -0400

    Add create_deck function

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,7 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for `Cards`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def create_deck do 
+    ["Ace", "Two", "Three"]
   end
 end
+
+# iex -S mix to start the shell
