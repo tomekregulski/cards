76d7cdb14e417f9274ca4cc3333d09ad1ea05dc9
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-04-14 19:34:17 -0400

    Add shuffle and contains functions.

--- lib/cards.ex
+++ lib/cards.ex
@@ -2,6 +2,14 @@ defmodule Cards do
   def create_deck do 
     ["Ace", "Two", "Three"]
   end
+
+  def shuffle(deck) do 
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
 end
 
 # iex -S mix to start the shell
