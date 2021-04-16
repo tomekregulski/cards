a7203e203437ecbdf3f2f8a493ac5c9be9346330
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-04-16 09:28:11 -0400

    Updated documentation

--- lib/cards.ex
+++ lib/cards.ex
@@ -15,6 +15,9 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Rearranges the list of playing cards into a new random order
+  """
   def shuffle(deck) do 
     Enum.shuffle(deck)
   end
@@ -39,7 +42,7 @@ defmodule Cards do
   ## Examples
 
       iex> deck = Cards.create_deck
-      iex> {hand, _deck} = Cards.deal(deck, 1)
+      iex> { hand, _deck } = Cards.deal(deck, 1)
       iex> hand
       ["Ace of Spades"]
 
@@ -48,10 +51,18 @@ defmodule Cards do
     Enum.split(deck, hand_size)
   end
 
+  @doc """
+    Saves the deck to a new file.
+  """
+
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
+
+   @doc """
+    Loads the specified file.
+  """
 
   def load(filename) do
     case File.read(filename) do
@@ -59,6 +70,19 @@ defmodule Cards do
       {:error, _reason} -> "That file does not exist"
     end
   end
+
+  @doc """
+    Using the pipe operator, this function first shuffles the deck, and then deals a hand according to the size set by the user. 
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> { hand, _deck } = Cards.create_hand(5)
+      iex> hand
+      ["King of Spades", "Jack of Hearts", "Four of Hearts", "Five of Diamonds",
+      "Queen of Spades"]
+
+  """
 
   def create_hand(hand_size) do
     Cards.create_deck
